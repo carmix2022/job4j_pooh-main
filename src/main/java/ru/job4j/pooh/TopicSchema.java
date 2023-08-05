e2b40782b90033e1f1e475de14d38bfd6f62bbe7
Илья <ily470@yandex.ru>
2023-08-05 15:33:31 +0700

    3. Тестовое задание - проект "Pooh JMS" [#268841  [#268841 #434750]]

---
+++ src/main/java/ru/job4j/pooh/TopicSchema.java
@@ -0,0 +1,53 @@
+package ru.job4j.pooh;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TopicSchema implements Schema {
+
+    private final ConcurrentHashMap<String, CopyOnWriteArrayList<Receiver>> receivers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, BlockingQueue<String>> data = new ConcurrentHashMap<>();
+    private final Condition condition = new Condition();
+
+    @Override
+    public void addReceiver(Receiver receiver) {
+        receivers.putIfAbsent(receiver.name(), new CopyOnWriteArrayList<>());
+        receivers.get(receiver.name()).add(receiver);
+        condition.on();
+    }
+
+    @Override
+    public void publish(Message message) {
+        data.putIfAbsent(message.name(), new LinkedBlockingQueue<>());
+        data.get(message.name()).add(message.text());
+        condition.on();
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            do {
+                for (String topic : data.keySet()) {
+                    var textQueue = data.get(topic);
+                    var topicReceivers = receivers.get(topic);
+                    if (topicReceivers != null) {
+                        String message = textQueue.poll();
+                        while (message != null) {
+                            String finalMessage = message;
+                            topicReceivers.forEach(x -> x.receive(finalMessage));
+                            message = textQueue.poll();
+                        }
+                    }
+                }
+                condition.off();
+            } while (condition.check());
+            try {
+                condition.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
